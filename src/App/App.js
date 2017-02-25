f086d0404f9ca2ef2ea0a2da373db67b0b78c2e7
Sarah Lim <sarah@sarahlim.com>
2017-02-25 14:18:16 -0600

    restructure component files

--- src/App/App.js
+++ src/App/App.js
@@ -1,3 +1,4 @@
+// @flow
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -8,7 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Perseus</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.

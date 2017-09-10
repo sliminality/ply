135d84f6de882759b47bc1f4a9ab4fe344d58e1d
Sarah Lim <sarah@sarahlim.com>
2017-09-09 18:31:45 -0700

    Actually check in filters so the build works

---
+++ src/models/filters.js
@@ -0,0 +1,28 @@
+// @flow
+import getDefaultValue, { CSSProperty } from './defaults';
+
+/**
+ * Allow users to define their own computed property filters
+ * for some aspect of interest.
+ */
+export type Filter = {
+  name: string,
+  props: Set<CSSProperty>,
+};
+
+/**
+ * Predicate for filtering down an array of [property, value] pairs.
+ */
+const filterPred = (fil: Filter) => ([prop, _]): boolean => fil.props.has(prop);
+
+/**
+ * Define some basic filters to start with.
+ */
+const FILTERS = {
+  layoutFilter: {
+    name: 'Layout',
+    props: new Set(['position', 'z-index']),
+  },
+};
+
+export { filterPred, FILTERS };
